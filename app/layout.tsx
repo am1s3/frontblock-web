52397bbbcd19285cb23fd04ec8954994fe414002
am1se <shushko.art@gmail.com>
2026-07-27 18:11:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,6 @@
 
+import "./globals.css";
+export const metadata = { title: "Front Block", description: "Military Minecraft server" };
+export default function Root({ children }: { children: React.ReactNode }) {
+  return <html lang="ru"><body>{children}</body></html>;
+}
